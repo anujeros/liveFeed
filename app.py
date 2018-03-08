36c18224cede383f4a1c17e0630344ca0647dc39
anujeros <anuj.sharma@eroslabs.co>
2018-03-08 15:36:39 +0530

    db conn

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request
-import logging, hashlib
+import logging, hashlib, MySQLdb
 from logging.handlers import RotatingFileHandler
 
 
@@ -10,29 +10,37 @@ app = Flask(__name__)
 
 @app.route('/liveStatus', methods=['POST'])
 def index():
+    app.logger.info(str(request.data))
+    print request.data
+    return 'JSON posted'
 
-    content = request.get_json()
-    token = "something"
-    timeToken = content['time'] + token
-    print timeToken
+    # content = request.get_json()
+    # token = "something"
+    # timeToken = content['time'] + token
+    # print timeToken
+    #
+    # print (content)
+    # m = hashlib.md5(timeToken)
+    # hashstr = m.hexdigest()
+    # print hashstr
+    # if hashstr == content['digest']:
+	# print 'Writing to logs'
+    # else:
+    #     return 'forbidden'
 
-    print (content)
-    m = hashlib.md5(timeToken)
-    hashstr = m.hexdigest()
-    print hashstr
-    if hashstr == content['digest']:
-	print 'Writing to logs'
-        app.logger.info(str(request.data))
-        return 'JSON posted'
-    else:
-        return 'forbidden'
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+def connection():
+    conn = MySQLdb.connect(host="localhost",
+                           user = "root",
+                           passwd = "anuj!",
+                           db = "liveFeed")
+    c = conn.cursor()
+
+    return c, conn
 
 if __name__ == "__main__":
     # initialize the log handler
-    logHandler = RotatingFileHandler('info.log', maxBytes=1000, backupCount=1)
+    logHandler = RotatingFileHandler('info.log', maxBytes=0, backupCount=0)
 
     # set the log handler level
     logHandler.setLevel(logging.INFO)
@@ -41,5 +49,5 @@ if __name__ == "__main__":
     app.logger.setLevel(logging.INFO)
 
     app.logger.addHandler(logHandler)
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
